124cd163ff9fab9700c141f66f065d3fdd6ad0ee
pechorka <pechor44@gmail.com>
2026-04-11 21:32:30 +0500

    sphere placement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use clap::Parser;
-
 use macroquad::prelude::*;
 
-#[derive(Parser)]
+#[derive(clap::Parser)]
 struct Config {
     #[arg(short = 'W', long, default_value_t = 1000)]
     w: usize,
@@ -12,13 +11,23 @@ struct Config {
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let cfg = Config::parse();
-    let mut image = Image::gen_image_color(cfg.w as u16, cfg.h as u16, YELLOW);
+    let cfg = Config::try_parse().unwrap_or_else(|e| e.exit());
+    let mut image = Image::gen_image_color(cfg.w as u16, cfg.h as u16, BLACK);
     let texture = Texture2D::from_image(&image);
+
+    let mut objs = Objects { spheres: vec![] };
 
     loop {
         if is_key_pressed(KeyCode::Escape) {
             return;
+        }
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let mouse = mouse_position_local();
+            objs.spheres.push(Sphere {
+                center: vec3(mouse.x, -mouse.y, 1.0),
+                radius: 0.3,
+            });
         }
 
         let render_ctx = RenderCtx {
@@ -27,7 +36,7 @@ async fn main() {
             elapsed: get_time() as f32,
         };
         let mut buf = image.get_image_data_mut();
-        render_sphere(&render_ctx, &mut buf);
+        render(&render_ctx, &mut buf, &objs);
 
         texture.update(&image);
         draw_texture(&texture, 0.0, 0.0, WHITE);
@@ -42,16 +51,23 @@ struct RenderCtx {
     elapsed: f32,
 }
 
-fn render_sphere(ctx: &RenderCtx, buf: &mut [[u8; 4]]) {
+struct Objects {
+    spheres: Vec<Sphere>,
+}
+
+struct Sphere {
+    center: Vec3,
+    radius: f32,
+}
+
+fn render(ctx: &RenderCtx, buf: &mut [[u8; 4]], objs: &Objects) {
     let w = ctx.w;
     let h = ctx.h;
 
     let origin = Vec3::ZERO;
 
-    let sc = vec3(0.0, 0.0, 1.0);
-    let sr: f32 = 0.3;
     let el = ctx.elapsed;
-    let lp = vec3(sc.x + sr * el.cos(), sc.y + sr * el.cos(), 0.3);
+    let lp = vec3(el.sin() * 1.0, el.cos() * -1.0, 1.0);
 
     let aspect = w as f32 / h as f32;
 
@@ -66,30 +82,29 @@ fn render_sphere(ctx: &RenderCtx, buf: &mut [[u8; 4]]) {
             let point = vec3(u * aspect, -v, 1.0);
             let dir = point - origin;
 
-            let a = dir.dot(dir);
-            let b = 2.0 * dir.dot(origin - sc);
-            let c = (origin - sc).dot(origin - sc) - sr * sr;
+            for s in objs.spheres.iter() {
+                let sc = s.center;
+                let sr = s.radius;
 
-            let d = b * b - 4.0 * a * c;
-            if d <= 0.0 {
-                continue;
+                let a = dir.dot(dir);
+                let b = 2.0 * dir.dot(origin - sc);
+                let c = (origin - sc).dot(origin - sc) - sr * sr;
+
+                let d = b * b - 4.0 * a * c;
+
+                if d > 0.0 {
+                    let t = (-b - d.sqrt()) / (2.0 * a);
+                    if t > 0.0 {
+                        let hit_point = origin + dir * t;
+                        let surface_normal = (hit_point - sc).normalize();
+
+                        let l = (lp - hit_point).normalize();
+
+                        let red = (200.0 * surface_normal.dot(l).max(0.0)).clamp(0.0, 255.0) as u8;
+                        buf[i] = [red, 0, 0, 255];
+                    }
+                }
             }
-
-            let t1 = (-b - d.sqrt()) / (2.0 * a);
-            let t2 = (-b + d.sqrt()) / (2.0 * a);
-            if t1 <= 0.0 && t2 <= 0.0 {
-                continue;
-            }
-
-            let t = if t1 <= 0.0 || t1 > t2 { t2 } else { t1 };
-
-            let hit_point = origin + dir * t;
-            let surface_normal = (hit_point - sc).normalize();
-
-            let l = (lp - hit_point).normalize();
-
-            let red = (200.0 * surface_normal.dot(l).max(0.0)).clamp(0.0, 255.0) as u8;
-            buf[i] = [red, 0, 0, 255];
         }
     }
 }
